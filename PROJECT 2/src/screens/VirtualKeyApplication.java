3b65d1866673eff6a0395324d71a4514cf9f56b6
lenovo <lenovo@DESKTOP-ELS3L7K>
2021-06-18 12:28:48 +0530

    project2

---
+++ PROJECT 2/src/screens/VirtualKeyApplication.java
@@ -0,0 +1,13 @@
+package screens;
+import screens.Welcome;
+
+public class VirtualKeyApplication {
+
+    public static void main(String[] args) {
+    	
+    	Welcome welcome = new Welcome();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
